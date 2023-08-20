9d5f3f84fe7aa591e5675cd61c27a84ab0ac82d4
hidalz <46967257+hidalz@users.noreply.github.com>
2023-08-20 15:56:40 +0100

    fix: refactor project structure

--- app/api/routes/projects.py
+++ app/api/routes/projects.py
@@ -3,19 +3,57 @@ from sqlalchemy.orm import Session
 from starlette.status import *
 
 import app.db.database as db
+import app.schemas.projects as project_schema
+import app.schemas.users as user_schema
 import app.services.projects as project_crud
-from app.models.projects import Project as project_model
-from app.schemas.projects import Project as project_schema
+from app.models.projects import (
+    Project as project_model,  # TODO: Why returning models y not schemas?
+)
 from app.services.auth import get_current_active_user
 
 router = APIRouter(tags=["Projects"], prefix="/projects")
 
 
-@router.get("/", response_model=list[project_schema])
-def read_projects(
+@router.get("/", status_code=HTTP_200_OK, response_model=list[project_schema.Project])
+def get_owned_and_collaborated_projects(
     skip: int = 0,
     limit: int = 100,
     db: Session = Depends(db.get_db),
-    current_user: project_model = Depends(get_current_active_user),
+    current_user: user_schema.User = Depends(get_current_active_user),
 ) -> list[project_model]:
-    return project_crud.get_projects(db, skip=skip, limit=limit)
+    return project_crud.get_owned_and_collaborated_projects(
+        db, user_id=current_user.id, skip=skip, limit=limit
+    )
+
+
+@router.post("/", status_code=HTTP_201_CREATED, response_model=project_schema.Project)
+def create_project(
+    project: project_schema.ProjectBase,
+    db: Session = Depends(db.get_db),
+    current_user: user_schema.User = Depends(get_current_active_user),
+) -> project_model:
+    return project_crud.create_project(db, project=project, user_id=current_user.id)
+
+
+# # Update project https://fastapi.tiangolo.com/tutorial/body-updates/
+# @router.put("/{project_id}", status_code=HTTP_200_OK, response_model=project_schema)
+# def update_project(
+#     project_name: str,
+#     project: project_schema,
+#     db: Session = Depends(db.get_db),
+#     current_user: user_schema.User = Depends(get_current_active_user),
+# ) -> project_model:
+#     db_project = project_crud.get_project_by_name(db, name=project_name)
+
+#     if not db_project:
+#         raise HTTPException(
+#             status_code=HTTP_404_NOT_FOUND,
+#             detail="Project not found",
+#         )
+
+#     if db_project.owner_id != current_user.id or current_user.id not in db_project.# type: ignore
+#         raise HTTPException(
+#             status_code=HTTP_403_FORBIDDEN,
+#             detail="Not enough permissions",
+#         )
+#     return project_crud.update_project(db, project=project, project_id=project_id)
--- app/services/projects.py
+++ app/services/projects.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 import app.models.projects as models
 import app.schemas.projects as schemas
+from app.models.relationships import ProjectCollaborators
 
 
 def get_project(db: Session, project_id: int) -> models.Project:
@@ -14,15 +15,45 @@ def get_project_by_name(db: Session, name: str) -> models.Project:
     return db.query(models.Project).filter(models.Project.name == name).first()
 
 
-def get_projects(db: Session, skip: int = 0, limit: int = 100) -> list[models.Project]:
-    return db.query(models.Project).offset(skip).limit(limit).all()
+def get_owned_and_collaborated_projects(
+    db: Session,
+    user_id: int,
+    skip: int = 0,
+    limit: int = 100,
+) -> list[models.Project]:
+    # Query for owned projects
+    owned_projects = (
+        db.query(models.Project)
+        .filter(models.Project.owner_id == user_id)
+        .offset(skip)
+        .limit(limit)
+    )
+
+    # Query for collaborated projects
+    collaborated_projects = (
+        db.query(models.Project)
+        .join(
+            ProjectCollaborators, models.Project.id == ProjectCollaborators.c.project_id
+        )
+        .filter(ProjectCollaborators.c.user_id == user_id)
+        .offset(skip)
+        .limit(limit)
+    )
+
+    # Combine the two queries using union
+    all_projects = owned_projects.union(collaborated_projects).all()
+
+    return all_projects
 
 
-def create_user_project(
+def create_project(
     db: Session, project: schemas.ProjectBase, user_id: int
 ) -> models.Project:
     db_project = models.Project(
-        **project.dict(), owner_id=user_id, created_at=datetime.now()
+        **project.dict(),
+        owner_id=user_id,
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
     )
 
     db.add(db_project)
@@ -39,6 +70,21 @@ def delete_project(db: Session, project_id: int):
 
     db.delete(db_project)
     db.commit()
+
+
+# def update_project(db: Session, project_id: int, project: schemas.ProjectBase):
+#     db_project = (
+#         db.query(models.Project).filter(models.Project.id == project_id).first()
+#     )
+
+#     db_project.name = project.name
+#     db_project.description = project.description
+#     db_project.updated_at = datetime.now()
+
+#     db.commit()
+#     db.refresh(db_project)
+
+#     return db_project
 
 
 # def archive_project(db: Session, project_id: int) -> models.Project:

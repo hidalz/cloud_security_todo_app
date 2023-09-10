f51163f49083b259c744c25c72737f9fc3d9cc65
moises.hidalgo <moises.hidalgo@partner.digitalchargingsolutions.com>
2023-09-10 13:40:29 +0200

    refactor: reestructure users-service (WIP)

---
+++ users-service/users_schemas.py
@@ -0,0 +1,44 @@
+"""Schemas for users service. 
+
+The schemas are used to define the structure of the data that is sent between the services (API)."""
+
+from pydantic import BaseModel, EmailStr
+
+from app.schemas.projects import Project
+from app.schemas.tasks import Task
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class User(UserBase):
+    # Notice that the User, the Pydantic model that will be used when reading a user
+    # (returning it from the API) doesn't include the password.
+    id: int
+    disabled: bool
+    tasks: list[Task] = []
+    owned_projects: list[Project] = []
+    collaborated_projects: list[Project] = []
+    is_superuser: bool = False
+
+    class Config:
+        orm_mode = True
+
+
+class UserInDB(UserBase):
+    # Notice that the UserInDB, the Pydantic model that will be used when writing a user
+    # (creating it in the database) includes the password.
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserUpdatePassword(BaseModel):
+    current_password: str
+    new_password: str
+
+    class Config:
+        orm_mode = True

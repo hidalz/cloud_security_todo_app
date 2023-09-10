513c9c9e98e1e743bbfaaff5447ea6fc65cca490
moises.hidalgo <moises.hidalgo@partner.digitalchargingsolutions.com>
2023-09-10 12:47:35 +0200

    feat: improve backend validations

--- app/services/validators.py
+++ app/services/validators.py
@@ -1,8 +1,6 @@
 import re
 
-from fastapi import (
-    HTTPException,  # Test validators instead of directly testing the api against different outcomes?? idk; TODO: Check if should do error handling in CRUD, routing or validators. Reuse them all in this module and raise exception to the routing
-)
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.models.projects import Project
@@ -12,9 +10,13 @@ from app.models.users import User
 # USER VALIDATORS
 
 
-def check_valid_email(email):
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        raise HTTPException(status_code=400, detail="Invalid email address")
+def check_valid_email(db: Session, email: str, check_email_is_registered: bool = True):
+    # Format checking is performed by Pydantic
+
+    db_email = db.query(User).filter(User.email == email).first()
+
+    if check_email_is_registered and db_email:
+        raise HTTPException(status_code=400, detail="Email already registered")
 
     return True
 
@@ -27,7 +29,7 @@ def check_valid_password(password):
 
     # Check for at least one number, one lowercase and one uppercase letter, and at least one special character
     if not re.match(
-        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]", password
+        r"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}|:;<>,.?/~])", password
     ):
         raise HTTPException(
             status_code=400,
@@ -37,11 +39,17 @@ def check_valid_password(password):
     return True
 
 
-def check_valid_username(username):
+def check_valid_username(db: Session, username: str):
     if len(username) < 4:
         raise HTTPException(
             status_code=400, detail="Username must be at least 4 characters long"
         )
+
+    # usernames are case insensitive
+    db_user = db.query(User).filter(User.username == username).first()
+
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
 
     return True
 
@@ -60,8 +68,35 @@ def check_valid_user_id(db: Session, user_id: int):
     return True
 
 
+def validate_user(
+    db: Session,
+    user_id: int | None = None,
+    username: str | None = None,
+    email: str | None = None,
+    check_email_is_registered: bool = True,
+    password: str | None = None,
+):
+    """It performs all the validations for a user depending on the CRUD method. It returns True if
+    all validations are passed, otherwise it raises an exception with details inside of each
+    validator function."""
+    if user_id:
+        check_valid_user_id(db=db, user_id=user_id)
+    if username:
+        check_valid_username(db=db, username=username)
+    if email:
+        check_valid_email(
+            db=db, email=email, check_email_is_registered=check_email_is_registered
+        )
+    if password:
+        check_valid_password(password=password)
+
+    return True
+
+
 # PROJECT VALIDATORS
-def check_valid_project_id(db: Session, project_id: int, return_db_project=False):
+def check_valid_project_id(
+    db: Session, project_id: int, return_db_project=False, user_id: int = None
+):
     if project_id < 0:
         raise HTTPException(
             status_code=400, detail="Project id must be greater or equal to 0"
@@ -71,6 +106,12 @@ def check_valid_project_id(db: Session, project_id: int, return_db_project=False
 
     if not db_project:
         raise HTTPException(status_code=404, detail="Project not found")
+
+    if user_id and db_project.owner_id != user_id:  # type: ignore
+        raise HTTPException(
+            status_code=403,
+            detail="Not enough permissions",
+        )
 
     # For the case of the update_project function, we want to return the db_project
     if return_db_project:
@@ -104,7 +145,7 @@ def check_valid_project_collaborator(db: Session, project_id: int, user_id: int)
     check_valid_user_id(db=db, user_id=user_id)
 
     db_project = check_valid_project_id(
-        db=db, project_id=project_id, return_db_project=True
+        db=db, project_id=project_id, return_db_project=True, user_id=user_id
     )
 
     if db_project.owner_id == user_id:  # type: ignore
@@ -130,17 +171,35 @@ def check_valid_project_collaborator(db: Session, project_id: int, user_id: int)
     return True
 
 
-# TASK VALIDATORS
+def validate_project(
+    db: Session,
+    project_id: int | None = None,
+    project_name: str | None = None,
+    user_id: int | None = None,
+):
+    """It performs all the validations for a project depending on the CRUD method. It returns True
+    if all validations are passed, otherwise it raises an exception with details inside of each
+    validator function."""
 
+    if project_id:
+        check_valid_project_id(db=db, project_id=project_id, user_id=user_id)
 
-def _check_valid_task_priority(task_priority: int):
-    if task_priority not in [1, 2, 3]:
-        raise HTTPException(status_code=400, detail="Task priority must be 1, 2 or 3")
+    if project_name and user_id:
+        check_valid_project_name(db=db, project_name=project_name, user_id=user_id)
+
+    if user_id:
+        check_valid_user_id(db=db, user_id=user_id)
 
     return True
 
 
-def _check_valid_task_id(db: Session, task_id: int):
+# TASK VALIDATORS
+def _check_valid_task_priority(task_priority: int):
+    if task_priority not in [1, 2, 3]:
+        raise HTTPException(status_code=400, detail="Task priority must be 1, 2 or 3")
+
+
+def _check_valid_task_id(db: Session, task_id: int, user_id: int):
     if task_id < 0:
         raise HTTPException(
             status_code=400, detail="Task id must be greater or equal to 0"
@@ -151,7 +210,9 @@ def _check_valid_task_id(db: Session, task_id: int):
     if not db_task:
         raise HTTPException(status_code=404, detail="Task not found")
 
-    return True
+    # Should not happen because of the foreign key constraint, but just in case
+    if db_task and db_task.owner_id != user_id:
+        raise HTTPException(status_code=403, detail="Not enough permissions")
 
 
 def _check_valid_task_parent_id(db: Session, task_parent_id: int):
@@ -165,8 +226,6 @@ def _check_valid_task_parent_id(db: Session, task_parent_id: int):
     if not db_task:
         raise HTTPException(status_code=404, detail="Task parent not found")
 
-    return True
-
 
 def validate_task(
     db: Session,
@@ -174,23 +233,22 @@ def validate_task(
     task_id: int | None = None,
     task_parent_id: int | None = None,
     task_priority: int | None = None,
+    user_id: int | None = None,
 ):
     """It performs all the validations for a task depending on the CRUD method. It returns True if
     all validations are passed, otherwise it raises an exception with details inside of each
     validator function."""
-    validations = {
-        "task_id": (task_id, _check_valid_task_id),
-        "task_parent_id": (task_parent_id, _check_valid_task_parent_id),
-        "task_priority": (task_priority, _check_valid_task_priority),
-        "project_id": (project_id, check_valid_project_id),
-    }
 
-    valid_results = []
-    for value, validator in validations.values():
-        if value is None:
-            valid_results.append(True)  # Treat None as valid
-        else:
-            # If an exception rises, the execution of the function will stop, and thus validation
-            valid_results.append(validator(db=db, value=value))
+    if task_priority:
+        _check_valid_task_priority(task_priority=task_priority)
 
-    return all(valid_results)
+    if task_id and user_id:
+        _check_valid_task_id(db=db, task_id=task_id, user_id=user_id)
+
+    if task_parent_id:
+        _check_valid_task_parent_id(db=db, task_parent_id=task_parent_id)
+
+    if project_id:
+        check_valid_project_id(db=db, project_id=project_id)
+
+    return True
